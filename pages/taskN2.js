0d5d7a79ac67be3c06ae351d09f4c6c90838caba
Nomioya <nomioyumshigen0117@gmail.com>
2025-02-14 14:44:44 +0800

    pushlehargauhaan

--- pages/taskN2.js
+++ pages/taskN2.js
@@ -1,23 +1,34 @@
-import React, {useEffect, useState} from "react";
+import { useState, useEffect } from "react";
 
-export default function setColorOnClick(){
-    const [color, setColor]=useState("");
-    const [data, setData] = useState();
-    
-    useEffect(()=>{
-        const fetchData = async()=>{
-            fetch('https://mongol-api-rest.vercel.app/clothes')
-                    .then(res => res.json())
-                    .then(json => console.log(json))
-        }
+export default function taskN2(){
+    const [data, setData] = useState("");
+    useEffect(() => {
+        const fetchData = async () =>{
+            try {
+                const response = await fetch (
+                    "https://mongol-api-rest.vercel.app/touristAttractions"
+                );
+                const result = await response.json();
+                setData(result);
+            } 
+            catch (error) {
+                console.log(error);
+            }
+        };
 
         fetchData();
-        console.log(fetchData);
-    },[]);
 
+    }, []);
+    
+    console.log(data);
     return(
-        <div>
-            
+        <div className="gird grid-cols-4 gap-4 m-4 bg-white">
+            {data?.touristAttractions?.map((item)=>(
+                <div>
+                    {item?.name}--------{item?.description}
+                </div>
+
+            ))}
         </div>
-    );  
-};  
+    );
+}
