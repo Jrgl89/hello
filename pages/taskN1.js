5527377ff94605737ea5bcbe0fb4bece6889e302
Zutguur <khanbileg7@gmail.com>
2025-02-21 14:54:59 +0800

    2025-02-21

--- pages/taskN1.js
+++ pages/taskN1.js
@@ -1,35 +1,32 @@
-import React, {useState} from "react";
-export default function TaskN1() {
-    const [click, setClick] =useState(0)
-    return(
-        <div className="bg-gray-100 h-screen w-screen p-4">
-            <div className="h-40 w-full  flex items-end">
-            <div className=" w-full p-4 bg-blue-600 flex justify-center">
-                <p className="text-3xl font-bold">Nest High school</p>
-            </div>
-            </div>
-            <div className="w-full h-24 flex justify-center items-center">
-                <p className="text-5xl font-bold text-black">Welcome to Nest school</p>
-            </div>
-            <div className="w-full h-20 flex justify-center">
-                <div className="w-[900px]">
-                <p className="text-slate-400 font-medium text-center text-xl">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
-                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley</p>
-                </div>
-            </div>
-            <div className="w-full h-12 bg-green-400 flex absolute">
-                <button className="rounded-lg bg-blue-700 py-1 px-6 font-bold" onClick={()=> setClick(click +1)}> Click Me</button>
-                <p>
-                    Clicked {click} times
-                </p>
-            </div>
+  import { useState, useEffect } from 'react';
+
+export default function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+   
+        const response = await fetch("https://mongol-api-rest.vercel.app/touristAttractions");
+        const result = await response.json();
+    
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="grid grid-cols-4 gap-4 m-4">
+      {data?.touristAttractions?.map((item, index) => (
+        <div key={index} className="p-4 border border-gray-300 rounded-lg">
+        
+            <img
+              src={item.image} 
+              className="w-full h-48 object-cover rounded-lg mb-2"
+            />
+          <h3 className="font-bold">{item?.name}</h3>
+          <p>{item?.description}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
-
- {data?.touristAttractions?.map((item)=>(
-                <div>
-                    {item?.name}--------{item?.description}
-                </div>
-
-            ))}
